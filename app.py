9b0f46e805fa2aeae55547809ddb1e1deb280786
fahime9407 <fahime.emlaei@gmail.com>
2026-08-12 15:36:08 +0330

    Update app.py

--- app.py
+++ app.py
@@ -7,28 +7,41 @@ import pandas as pd
 
 
 window = tk.Tk() # Create a window
-
-movieDf = pd.read_csv("movies.csv")
-
 window.title("Movie Recommender") # Title of the window
-window.geometry("700x600") # Size of the window
+window.geometry("800x500") # Size of the window
+
+movie_df = pd.read_csv("movies.csv")
+
 
 def search_movie(event):
     search_text = search_entry.get().strip() # Read the input text
-    movie_listbox.delete(0, tk.END) # Clear the listbox before adding the new movie results
+    search_listbox.delete(0, tk.END) # Clear the listbox before adding the new movie results
 
     if search_text: # Check that the input is not empty
-        results = movieDf[movieDf.title.str.contains(search_text, case=False, na=False)] # Check the movie titles and select the ones that contain the entered text
+        results = movie_df[movie_df.title.str.contains(search_text, case=False, na=False)] # Check the movie titles and select the ones that contain the entered text
 
         for movie in results["title"].head(20): # Display only the first 20 movies from the search results
-            movie_listbox.insert(tk.END, movie)
+            search_listbox.insert(tk.END, movie)
 
-def say_hello():
+
+def add_movie():
     movie = search_entry.get().strip() # Read the input text
 
     if movie: # Check that the input is not empty
-        movie_listbox.insert(tk.END, movie) # Add it to the list
+        selected_listbox.insert(tk.END, movie) # Add it to the list
         search_entry.delete(0, tk.END) # Clear the input field
+
+
+def select_movie(event):
+    selcted = search_listbox.curselection() # Get the index of the selected item
+
+    if selcted: # If the tuple is not empty
+        selcted_index = selcted[0]
+        selected_title = search_listbox.get(selcted_index) # Get the item at the specified index
+
+        search_entry.delete(0, tk.END) # Clear the input field
+        search_entry.insert(0, selected_title) # Insert the selected movie title at index 0 of the entry
+
 
 title_label = tk.Label(window, text="Movie Recommendation System", font=("Arial", 20), fg="navy") # Add title as a label
 title_label.grid(row=0, column=0)
@@ -40,13 +53,20 @@ search_entry = tk.Entry(window, width=40)
 search_entry.grid(row=1, column=1)
 search_entry.bind("<KeyRelease>", search_movie) # Call the 'search_movie' function when a key is released in the search entry
 
-# This button adds the entered movie and rating to the list, then clears the input fields for the next movie and rating
-add_button = tk.Button(window, text="Add Movie", command=say_hello)
-add_button.grid(row=2, column=1)
+# This button adds the entered movie and rating to the selected list, then clears the input fields for the next movie and rating
+add_button = tk.Button(window, text="Add Movie", command=add_movie)
+add_button.grid(row=1, column=2)
+
+# As the user types in the search entry, continuously search the movie dataset and display the search results in the listbox
+search_listbox = tk.Listbox(window, width=50, height=8)
+search_listbox.grid(row=3, column=1)
+search_listbox.bind("<<ListboxSelect>>", select_movie)
 
 # When the user clicks the "Add Movie" button, the entered movie and rating are added to this list
-movie_listbox = tk.Listbox(window, width=50, height=8)
-movie_listbox.grid(row=3, column=0)
+selected_listbox = tk.Listbox(window, width=50, height=8)
+selected_listbox.grid(row=3, column=0)
+
+
 
 
 window.mainloop() # Wait for user
